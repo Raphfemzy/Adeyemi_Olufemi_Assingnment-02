9bb46603b8a39a93ab8ce74cc0697b145a24d454
Raphfemzy <109247089+Raphfemzy@users.noreply.github.com>
2022-07-15 00:22:49 +0000

    I returned the sum of numbers in an array

--- src/assignment.js
+++ src/assignment.js
@@ -13,7 +13,7 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
- let arrayOfNumbers = [ 19, 20, 409, 30.21];
+ //let arrayOfNumbers = [ 19, 20, 409, 30.21];
 function sumOfNumbers(arrayOfNumbers) {
 let sum = 0;
   for (let index = 0; index < arrayOfNumbers.length; index++) {
@@ -22,7 +22,7 @@ let sum = 0;
   console.log(sum)
     return arrayOfNumbers.length;
 }
- sumOfNumbers(arrayOfNumbers)
+ //sumOfNumbers(arrayOfNumbers)
 assignment.sumOfNumbers = sumOfNumbers;
 
 /**
